2a9c513c3b9eee874d9fdf3b39698381c015bd48
Aaliyah Harper <aaliyah@aileks.dev>
2025-11-12 21:42:11 -0500

    new config

--- init.lua
+++ init.lua
@@ -1,17 +1,162 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.o.swapfile = false
+vim.o.clipboard = "unnamedplus"
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.showtabline = 2
+vim.o.signcolumn = "yes"
+vim.o.winborder = "rounded"
+vim.o.cursorcolumn = false
+vim.o.ignorecase = true
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.undofile = true
 
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("plugins.init")
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/aznhe21/actions-preview.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/mfussenegger/nvim-jdtls" }
+})
+
+require "mason".setup()
+
+local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		preview = { treesitter = false },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"─", -- top
+			"│", -- right
+			"─", -- bottom
+			"│", -- left
+			"┌", -- top-left
+			"┐", -- top-right
+			"┘", -- bottom-right
+			"└", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
+telescope.load_extension("ui-select")
+
+require("actions-preview").setup {
+	backend = { "telescope" },
+	telescope = vim.tbl_extend(
+		"force",
+		require("telescope.themes").get_dropdown(), {}
+	)
+}
+
+require("oil").setup({
+	lsp_file_methods = {
+		enabled = true,
+		timeout_ms = 1000,
+		autosave_changes = true,
+	},
+	columns = {
+		"permissions",
+		"icon",
+	},
+	float = {
+		max_width = 0.7,
+		max_height = 0.6,
+		border = "rounded",
+	},
+})
+
+require("vague").setup({ transparent = true })
+require("luasnip").setup({ enable_autosnippets = true })
+
+vim.lsp.enable({
+	"lua_ls", "cssls", "tinymist", "ts_ls",
+	"tailwindcss", "emmet_ls", "jdtls"
+})
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+
+
+local function pack_clean()
+	local active_plugins = {}
+	local unused_plugins = {}
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		active_plugins[plugin.spec.name] = plugin.active
+	end
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not active_plugins[plugin.spec.name] then
+			table.insert(unused_plugins, plugin.spec.name)
+		end
+	end
+
+	if #unused_plugins == 0 then
+		print("No unused plugins.")
+		return
+	end
+
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+	if choice == 1 then
+		vim.pack.del(unused_plugins)
+	end
+end
+
+local builtin = require("telescope.builtin")
+local actions = require("actions-preview")
+
+function all_files() builtin.find_files({ no_ignore = true }) end
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<Esc>", ":nohl<CR>", { silent = true })
+vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>pc", pack_clean)
+vim.keymap.set("n", "<leader>f", builtin.find_files)
+vim.keymap.set("n", "<leader>h", builtin.help_tags)
+vim.keymap.set("n", "<leader>g", builtin.live_grep)
+vim.keymap.set("n", "<leader>b", builtin.buffers)
+vim.keymap.set("n", "<leader>F", all_files)
+vim.keymap.set("n", "<leader>ca", actions.code_actions)
+vim.keymap.set("n", "<C-f>", ":Open .<CR>")
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
+vim.cmd("hi TabLineFill guibg=NONE")
+vim.cmd("set completeopt+=menuone,noselect,popup")
