b5cc9058ec30f6200c92e5a0fe8c78067c2131cf
Aaliyah Harper <aaliyah@aileks.dev>
2025-11-13 10:58:02 -0500

    add blink over native completion

--- init.lua
+++ init.lua
@@ -34,199 +34,190 @@ vim.o.winborder = "rounded"
 
 -- Plugins and setup
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/mbbill/undotree" },
-	{ src = "https://github.com/aznhe21/actions-preview.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
-	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
-	{ src = "https://github.com/pmizio/typescript-tools.nvim" },
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
-	{ src = "https://github.com/windwp/nvim-autopairs" },
-	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
+    { src = "https://github.com/vague2k/vague.nvim" },
+    { src = "https://github.com/nvim-lualine/lualine.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/mbbill/undotree" },
+    { src = "https://github.com/aznhe21/actions-preview.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/mfussenegger/nvim-jdtls" },
+    { src = "https://github.com/pmizio/typescript-tools.nvim" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = "https://github.com/windwp/nvim-autopairs" },
+    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+    { src = "https://github.com/saghen/blink.cmp", version = "1.7.0" },
 })
 
 require("lualine").setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-		},
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        },
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    winbar = {},
+    inactive_winbar = {},
+    extensions = {},
 })
 
 require("ibl").setup({
-	indent = { char = "│" },
-	scope = { enabled = false },
+    indent = { char = "│" },
+    scope = { enabled = false },
 })
 
 require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+        },
+    },
 })
 
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"lua_ls",
-		"cssls",
-		"tinymist",
-		"tailwindcss",
-		"emmet_ls",
-		"jdtls",
-		"jsonls",
-	},
+    ensure_installed = {
+        "lua_ls",
+        "cssls",
+        "tinymist",
+        "tailwindcss",
+        "emmet_ls",
+        "jdtls",
+        "jsonls",
+    },
 })
 
 vim.lsp.enable({
-	"lua_ls",
-	"cssls",
-	"tinymist",
-	"tailwindcss",
-	"emmet_ls",
-	"jdtls",
-	"prettier",
-	"google-java-format",
-	"xmlformatter",
-	"shfmt",
-	"marksman",
+    "lua_ls",
+    "cssls",
+    "tinymist",
+    "tailwindcss",
+    "emmet_ls",
+    "jdtls",
+    "prettier",
+    "google-java-format",
+    "xmlformatter",
+    "shfmt",
+    "marksman",
+})
+
+-- Fixes lua_ls warnings in nvim config files
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+        },
+    },
 })
 
 require("typescript-tools").setup({
-	settings = {
-		tsserver_file_preferences = {
-			includeInlayParameterNameHints = "all",
-			includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-			includeInlayFunctionParameterTypeHints = true,
-			includeInlayVariableTypeHints = true,
-			includeInlayPropertyDeclarationTypeHints = true,
-			includeInlayFunctionLikeReturnTypeHints = true,
-			includeInlayEnumMemberValueHints = true,
-		},
-	},
+    settings = {
+        tsserver_file_preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+        },
+    },
+})
+
+require('blink.cmp').setup({
+    build = "cargo build --release",
+    completion = {
+        menu = {
+            border = "rounded",
+        },
+    },
+    keymap = {
+        preset = "default",
+    },
+    sources = {
+        default = { "lsp", "path", "buffer" },
+    },
 })
 
 local telescope = require("telescope")
 telescope.setup({
-	defaults = {
-		preview = { treesitter = false },
-		color_devicons = true,
-		sorting_strategy = "ascending",
-		path_displays = { "smart" },
-		layout_config = {
-			height = 100,
-			width = 400,
-			prompt_position = "bottom",
-			preview_cutoff = 40,
-		},
-	},
+    defaults = {
+        preview = { treesitter = false },
+        color_devicons = true,
+        sorting_strategy = "ascending",
+        path_displays = { "smart" },
+        layout_config = {
+            height = 100,
+            width = 400,
+            prompt_position = "bottom",
+            preview_cutoff = 40,
+        },
+    },
 })
 telescope.load_extension("ui-select")
 telescope.load_extension("fzf")
 
 require("actions-preview").setup({
-	backend = { "telescope" },
-	telescope = vim.tbl_extend("force", require("telescope.themes").get_dropdown(), {}),
+    backend = { "telescope" },
+    telescope = vim.tbl_extend("force", require("telescope.themes").get_dropdown(), {}),
 })
 
 require("oil").setup({
-	lsp_file_methods = {
-		enabled = true,
-		timeout_ms = 1000,
-		autosave_changes = true,
-	},
-	columns = {
-		"permissions",
-		"icon",
-	},
-	float = {
-		max_width = 0.7,
-		max_height = 0.6,
-		border = "rounded",
-	},
-})
-
-require("conform").setup({
-	formatters = {
-		["google-java-format"] = {
-			prepend_args = { "--aosp" },
-			args = { "--replace", "$FILENAME" },
-			stdin = false,
-			require_cwd = true,
-		},
-	},
-	formatters_by_ft = {
-		javascript = { "prettier" },
-		javascriptreact = { "prettier" },
-		typescript = { "prettier" },
-		typescriptreact = { "prettier" },
-		json = { "prettier" },
-		yaml = { "prettier" },
-		markdown = { "prettier" },
-		html = { "prettier" },
-		css = { "prettier" },
-		scss = { "prettier" },
-		lua = { "stylua" },
-		sh = { "shfmt" },
-		bash = { "shfmt" },
-		zsh = { "shfmt" },
-		java = { "google-java-format" },
-		xml = { "xmlformatter" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
-	notify_on_error = true,
-	notify_no_formatters = true,
+    lsp_file_methods = {
+        enabled = true,
+        timeout_ms = 1000,
+        autosave_changes = true,
+    },
+    columns = {
+        "permissions",
+        "icon",
+    },
+    float = {
+        max_width = 0.7,
+        max_height = 0.6,
+        border = "rounded",
+    },
 })
 
 require("nvim-autopairs").setup()
@@ -235,28 +226,28 @@ require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip").setup({ enable_autosnippets = true })
 
 local function pack_clean()
-	local active_plugins = {}
-	local unused_plugins = {}
+    local active_plugins = {}
+    local unused_plugins = {}
 
-	for _, plugin in ipairs(vim.pack.get()) do
-		active_plugins[plugin.spec.name] = plugin.active
-	end
+    for _, plugin in ipairs(vim.pack.get()) do
+        active_plugins[plugin.spec.name] = plugin.active
+    end
 
-	for _, plugin in ipairs(vim.pack.get()) do
-		if not active_plugins[plugin.spec.name] then
-			table.insert(unused_plugins, plugin.spec.name)
-		end
-	end
+    for _, plugin in ipairs(vim.pack.get()) do
+        if not active_plugins[plugin.spec.name] then
+            table.insert(unused_plugins, plugin.spec.name)
+        end
+    end
 
-	if #unused_plugins == 0 then
-		print("No unused plugins.")
-		return
-	end
+    if #unused_plugins == 0 then
+        print("No unused plugins.")
+        return
+    end
 
-	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
-	if choice == 1 then
-		vim.pack.del(unused_plugins)
-	end
+    local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+    if choice == 1 then
+        vim.pack.del(unused_plugins)
+    end
 end
 
 -- Autocmds
@@ -264,41 +255,31 @@ local default_color = "vague"
 local color_group = vim.api.nvim_create_augroup("colors", { clear = true })
 
 vim.api.nvim_create_autocmd("TabEnter", {
-	group = color_group,
-	callback = function(_args)
-		if vim.t.color then
-			vim.cmd("colorscheme " .. vim.t.color)
-		else
-			vim.cmd("colorscheme " .. default_color)
-		end
-	end,
+    group = color_group,
+    callback = function(_args)
+        if vim.t.color then
+            vim.cmd("colorscheme " .. vim.t.color)
+        else
+            vim.cmd("colorscheme " .. default_color)
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-	group = color_group,
-	callback = function(_args)
-		if vim.t.color then
-			vim.cmd("colorscheme " .. vim.t.color)
-		else
-			vim.cmd("colorscheme " .. default_color)
-		end
-	end,
+    group = color_group,
+    callback = function(_args)
+        if vim.t.color then
+            vim.cmd("colorscheme " .. vim.t.color)
+        else
+            vim.cmd("colorscheme " .. default_color)
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("my.lsp", {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client:supports_method("textDocument/completion") then
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		end
-	end,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- Keybinds
@@ -307,7 +288,7 @@ local actions = require("actions-preview")
 local ls = require("luasnip")
 
 function all_files()
-	builtin.find_files({ no_ignore = true })
+    builtin.find_files({ no_ignore = true })
 end
 
 vim.g.mapleader = " "
@@ -318,6 +299,7 @@ vim.keymap.set("n", "<leader>q", ":quit<CR>", { silent = true })
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>e", ":Oil<CR>", { silent = true })
 vim.keymap.set("n", "<leader>pc", pack_clean)
+vim.keymap.set('n', '<leader>ps', vim.pack.update)
 vim.keymap.set("n", "<leader>f", builtin.find_files)
 vim.keymap.set("n", "<leader>h", builtin.help_tags)
 vim.keymap.set("n", "<leader>g", builtin.live_grep)
@@ -342,13 +324,13 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set({ "i", "s" }, "<C-e>", function()
-	ls.expand_or_jump(1)
+    ls.expand_or_jump(1)
 end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-J>", function()
-	ls.jump(1)
+    ls.jump(1)
 end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-K>", function()
-	ls.jump(-1)
+    ls.jump(-1)
 end, { silent = true })
 
 vim.keymap.set("v", ">", ">gv")
@@ -356,11 +338,10 @@ vim.keymap.set("v", "<", "<gv")
 
 -- Easy tab switching
 for i = 1, 8 do
-	vim.keymap.set({ "n", "t" }, "<Leader>" .. i, "<Cmd>tabnext " .. i .. "<CR>")
+    vim.keymap.set({ "n", "t" }, "<Leader>" .. i, "<Cmd>tabnext " .. i .. "<CR>")
 end
 
 -- Cmd settings
 vim.cmd("colorscheme " .. default_color)
 vim.cmd("hi statusline guibg=NONE")
 vim.cmd("hi TabLineFill guibg=NONE")
-vim.cmd("set completeopt+=menuone,noselect,popup")
