86da547628dd09e9ff73c771c550cc9fa8d1672d
Kieran Pedrick <kieran@aileks.dev>
2026-09-02 11:27:08 -0400

    fix todo highlighting

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -9,6 +9,13 @@ todo.setup({
   colors = {
     test = { '@label', 'Identifier' },
   },
+  -- default patterns require a colon after the keyword; match bare keywords too
+  highlight = {
+    pattern = [[.*<(KEYWORDS)>]],
+  },
+  search = {
+    pattern = [[\b(KEYWORDS)\b]],
+  },
 })
 
 vim.keymap.set('n', ']t', todo.jump_next, { desc = 'Next todo comment' })
